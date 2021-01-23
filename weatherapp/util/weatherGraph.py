3c0307c4389ee6de9bb28c491617622e3fd40396
TwoSails <49128313+twosails@users.noreply.github.com>
2021-01-23 22:03:58 +0000

    Started adding support for Django Plotly Dash Added Scatter Polar graph for wind direction

--- weatherapp/util/weatherGraph.py
+++ weatherapp/util/weatherGraph.py
@@ -3,8 +3,15 @@ import datetime
 import os
 
 from plotly.offline import plot
-from plotly.graph_objs import Scatter, Bar
+from plotly.graph_objs import Scatter, Bar, Scatterpolar
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from plotly.subplots import make_subplots
+
+from django_plotly_dash import DjangoDash
 import plotly.graph_objects as go
+from ..templatetags.wind_extra import wind_direction as windDir
 
 
 class WeatherGraph:
@@ -18,6 +25,18 @@ class WeatherGraph:
         self.dataHumidity = None
         self.dataRainfall = None
 
+        self.wind_dirs = {0: "N",
+                          45: "NE",
+                          90: "E",
+                          135: "SE",
+                          180: "S",
+                          225: "SW",
+                          270: "W",
+                          315: "NW",
+                          360: "N"}
+
+        self.degrees = [0, 45, 90, 135, 180, 225, 270, 315, 360]
+
         self.url = self.config["settings"]["apiURL"]
         self.urlAddition = "/weather/data"
 
@@ -30,11 +49,48 @@ class WeatherGraph:
         TIMESTAMPS = [datetime.datetime.fromtimestamp(x) for x in self.dataWind[3]["TIMESTAMPS"][::-1]]
         windDirection = self.dataWind[1]["direction"][::-1]
 
-        wind_direction_graph = plot([Scatter(x=TIMESTAMPS, y=windDirection, mode="lines", name="Wind Direction")],
-                                    output_type="div",
-                                    include_plotlyjs=False, show_link=False, link_text="")
+        windDirectionGraph = plot([Scatterpolar(r=self.dataWind[0]["speed"][::-1], theta=windDirection, mode='markers',
+                                                hovertext=["True", "False"], text=["Speed", "Wind Direction"])],
+                                  output_type="div", include_plotlyjs=False, show_link=False, link_text="")
 
-        return wind_direction_graph
+        fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'polar'}, {'type': 'polar'}]])
+
+        rSpeed = self.dataWind[0]["speed"][::-1]
+        rGust = self.dataWind[2]["gust"][::-1]
+        theta = windDirection
+
+        fig.add_trace(Scatterpolar(mode="markers", r=rSpeed, theta=theta,
+                                   marker=dict(color='lightblue', size=8, symbol='square')), 1, 1)
+        fig.add_trace(Scatterpolar(mode="markers", r=rGust, theta=theta,
+                                   marker=dict(color='lightblue', size=8, symbol='square')), 1, 2)
+
+        #  fig.update_traces(r=r, theta=theta, mode="markers", marker=dict(color='lightslategray', size=8, symbol='square'))
+
+        fig.update_layout(
+            title="Wind Gust/Wind Average Speed in Wind Direction",
+            showlegend=False,
+            polar=dict(
+                radialaxis_tickfont_size=8,
+                angularaxis=dict(
+                    tickfont_size=8,
+                    rotation=90,  # start position of angular axis
+                    direction="clockwise"
+                )
+            ),
+            polar2=dict(
+                radialaxis_tickfont_size=8,
+                angularaxis=dict(
+                    tickfont_size=8,
+                    rotation=90,
+                    direction="clockwise"
+                ),
+            ))
+
+        app = DjangoDash("windDirection")
+        app.layout = html.Div([dcc.Graph(figure=fig)])
+
+        # windDirectionGraph = plot([fig], output_type="div", include_plotlyjs=False)
+        return None
 
     def wind_speed_graph(self):
         TIMESTAMPS = [datetime.datetime.fromtimestamp(x) for x in self.dataWind[3]["TIMESTAMPS"][::-1]]
