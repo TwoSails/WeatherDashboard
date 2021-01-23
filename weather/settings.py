50dd12cb0a29b4c12c7faa35b1c7edd6a85af29d
TwoSails <49128313+twosails@users.noreply.github.com>
2021-01-23 19:00:53 +0000

    Allowed all hosts

--- weather/settings.py
+++ weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = get_config()["dashboard"]["settings"]["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
