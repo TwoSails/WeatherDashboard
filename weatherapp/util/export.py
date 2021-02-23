c9c7926e3dab03e881fda952a72bdc8d61a10557
TwoSails <49128313+twosails@users.noreply.github.com>
2021-02-23 22:22:27 +0000

    Fixed download data span error
    Signed-off-by: TwoSails <unconfigured@null.spigotmc.org>

--- weatherapp/util/export.py
+++ weatherapp/util/export.py
@@ -23,23 +23,23 @@ class FileExport:
 
     def content_data(self):
         self.content['windDirection'] = self.data.data['windDirection'][
-                                        :(self.post.span_int * 48)] if self.post.wind_direction else None
+                                        (self.post.span_int * 48):] if self.post.wind_direction else None
         self.content['avgWindSpeed'] = self.data.data['avgWindSpeed'][
-                                       :(self.post.span_int * 48)] if self.post.avg_wind_speed else None
+                                       (self.post.span_int * 48):] if self.post.avg_wind_speed else None
         self.content['windGust'] = self.data.data['windGust'][
-                                   :(self.post.span_int * 48)] if self.post.wind_gust else None
+                                   (self.post.span_int * 48):] if self.post.wind_gust else None
         self.content['rainfall'] = self.data.data['rainfall'][
-                                   :(self.post.span_int * 48)] if self.post.rainfall else None
+                                   (self.post.span_int * 48):] if self.post.rainfall else None
         self.content['humidity'] = self.data.data['humidity'][
-                                   :(self.post.span_int * 48)] if self.post.humidity else None
+                                   (self.post.span_int * 48):] if self.post.humidity else None
         self.content['ambientTemp'] = self.data.data['ambientTemp'][
-                                      :(self.post.span_int * 48)] if self.post.ambient_temp else None
+                                      (self.post.span_int * 48):] if self.post.ambient_temp else None
         self.content['groundTemp'] = self.data.data['groundTemp'][
-                                     :(self.post.span_int * 48)] if self.post.ground_temp else None
+                                     (self.post.span_int * 48):] if self.post.ground_temp else None
         self.content['pressure'] = self.data.data['pressure'][
-                                   :(self.post.span_int * 48)] if self.post.pressure else None
+                                   (self.post.span_int * 48):] if self.post.pressure else None
         self.content['timestamps'] = self.data.data['TIMESTAMPS'][
-                                     :(self.post.span_int * 48)] if self.post.timestamps else None
+                                     (self.post.span_int * 48):] if self.post.timestamps else None
 
     def create_file(self):
         file = open(self.filename, 'w')
@@ -49,7 +49,6 @@ class FileExport:
     def parse(self):
         for x in self.content_items:
             if self.content[x] is None:
-                print('pop')
                 self.content = self.removeKey(self.content, x)
             else:
                 pass
